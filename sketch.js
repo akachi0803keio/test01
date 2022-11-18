6225bedc7fb0f33801f78e2437f3865adf068e18
akachi0803keio <118536221+akachi0803keio@users.noreply.github.com>
2022-11-18 22:28:42 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,7 @@
 void setup(){
-  let PImage = nail[];
-　　　　let frame = 0;
-  nail = new PImage[44];
-  size(640,480);
   print('test page');
-  for(int i =0; i<44; i++)
-    nail[i] =loadImage("11-image_"+i+".jpg");
-  
 }
 
 void draw(){
-  image(nail[frame], 0, 0);
-  if(mousePressed){
-    frame=frame+1;
-  }else{
-    frame =frame-1;
-  }
   
-  if(frame <0){
-    frame =0;
-  }
-    if(frame >43){
-      frame =43;
-  }
 }
