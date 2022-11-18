fae7af81d322841cd104d0e70aa088aab594e9dc
akachi0803keio <118536221+akachi0803keio@users.noreply.github.com>
2022-11-18 22:45:06 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,15 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    print('aaa');
+    createCanvas(400, 400);
+    noLoop();
 }
- 
+
 function draw() {
-    ellipse(100, 100, 80, 50);
+    background(255);
+    fill(255);
+    stroke(50);
+    strokeWeight(8);
+    ellipse(200, 200, 200, 200);
+    ellipse(300, 200, 200, 200);
+    ellipse(200, 300, 200, 200);
+    ellipse(300, 300, 200, 200);
 }
